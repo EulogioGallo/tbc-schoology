f1c4591bc01787a385b03e99af554abc098dc911
Gallo <egallo@broadcenter.org>
2017-05-31 15:27:53 -0700

    Making sure our existing structure can handle the schoology API calls

--- web/index.php
+++ web/index.php
@@ -3,15 +3,6 @@
 // for heroku
 require('../vendor/autoload.php');
  
-// for local execution
-/*
-require('../vendor/oauth-io/oauth/src/OAuth_io/OAuth.php');
-require('../vendor/oauth-io/oauth/src/OAuth_io/RequestObject.php');
-require('../vendor/oauth-io/oauth/src/OAuth_io/NotInitializedException.php');
-require('../vendor/oauth-io/oauth/src/OAuth_io/NotAuthenticatedException.php');
-require('../vendor/oauth-io/oauth/src/OAuth_io/Injector.php');
-require('../vendor/oauth-io/oauth/src/OAuth_io/HttpWrapper.php');
-*/
 require_once('../schoology_php_sdk-master/SchoologyApi.class.php');
 require_once('../schoology_php_sdk-master/SchoologyContentApi.class.php');
 require_once('../schoology_php_sdk-master/SchoologyExceptions.php');
@@ -20,12 +11,10 @@ require_once('../salesforce/soapclient/SforcePartnerClient.php');
 require_once('Schoology_OAuth.php');
 
  
-// use OAuth_io\OAuth;
- 
- 
  $SchoologyApi = new SchoologyContainer();
  error_log(print_r($SchoologyApi, true));
  $SchoologyApi->schoologyOAuth();
  error_log(print_r($SchoologyApi, true));
- 
+ $api_result = $SchoologyApi->schoology->apiResult('users/22135108/sections');
+ error_log(print_r($api_result, true));
 ?>
