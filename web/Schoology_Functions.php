b953332d21b544a84659940dc5d89b1583b023c3
Gallo <egallo@broadcenter.org>
2017-06-06 13:59:40 -0700

    hmm

--- web/Schoology_Functions.php
+++ web/Schoology_Functions.php
@@ -1,11 +1,11 @@
 <?php
 
 class Schoology_Functions {
-	public static function sendAssignmentMaster(Object $jsonObject) {
+	public static function sendAssignmentMaster(stdClass $jsonObject) {
 		error_log(print_r(jsonObject,true));
 	}
 	
-	public static function sendAssignmentToSF(Object $jsonObject) {
+	public static function sendAssignmentToSF(stdClass $jsonObject) {
 		error_log(print_r(jsonObject,true));
 	}
 }
