834712ba2fbc626e1e541221258ee40a64590103
Gallo <egallo@broadcenter.org>
2017-06-07 10:51:55 -0700

    Checking assignment JSON format

--- web/index.php
+++ web/index.php
@@ -6,8 +6,13 @@
  
 $SchoologyApi = new SchoologyContainer();
 $SchoologyApi->schoologyOAuth();
+// Lets list all assignments to see how they're structured in Schoology
+$api_result = $SchoologyApi->schoology->api('/sections/1102963310/assignments?with_attachments=1');
+
+/*
 $json_result = json_decode(file_get_contents("php://input"));
 error_log(print_r($json_result,true));
 Schoology_Functions::sendAssignmentToSF($json_result);
+*/
 
 ?>
