a4764eec0ddcfe1a3c17eb57c52ab53eaabc287b
Lopez <elopez@broadcenter.org>
2017-07-06 11:58:34 -0700

    bl

--- web/Schoology_OAuth.php
+++ web/Schoology_OAuth.php
@@ -533,7 +533,7 @@
 				throw new Exception('Could not get Assignment Submission');
 			}
 
-			$query2 = $this->storage->db->prepare("UPDATE salesforce.ram_assignment__c SET submission_date_time__c  = time(), WHERE (schoology_assignment_id__c = :schoologyAssId) AND (schoology_user_id__c = :schoologyUserId)"); //sync to schoology?
+			$query2 = $this->storage->db->prepare("UPDATE salesforce.ram_assignment__c SET submission_date_time__c  = time() WHERE (schoology_assignment_id__c = :schoologyAssId) AND (schoology_user_id__c = :schoologyUserId)"); //sync to schoology?
 
 			if($query2->execute(array(':schoologyAssId' => $schoologyAssId , ':schoologyUserId' => $schoologyUserId))) {
 				error_log('Successful Query Call ');
