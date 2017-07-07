acb859ebb9c1e6064d2598b0e905f09ce0f48a5d
Lopez <elopez@broadcenter.org>
2017-07-07 14:48:44 -0700

    blah

--- web/Schoology_OAuth.php
+++ web/Schoology_OAuth.php
@@ -511,7 +511,7 @@
 				$schoologyUserId= $thisAss->object->uid;
 				$timeStamp = current($thisAss->object->attachments->files->file)->timestamp;
 
-				$subDate1 = (string) Date('Y-m-d H:i:s', $timeStamp);
+				$subDate1 = $timeStamp;
 
 
 				//$subDate = new DateTime();
